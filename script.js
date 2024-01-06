36b6d01a7ea3d1038dfa30cfe5cd52120c0aa1d0
kushigowdak <153368743+kushigowdak@users.noreply.github.com>
2024-01-06 09:18:29 +0530

    final resume

---
+++ script.js
@@ -0,0 +1,7 @@
+document
+    .getElementById("contactme")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      alert("Thank you for visiting my profile🙏!");
+  });
+  
